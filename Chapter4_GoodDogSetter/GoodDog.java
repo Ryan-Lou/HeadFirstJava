018acceb2a8fb95a54ab11de6742bbfc0a7a6cd7
Ryan-Lou <1874216575@qq.com>
2023-07-15 09:30:50 +0800

    BetterChapter4
    BetterChapter4 by ChatGPT
    使sizes随机为0-100的数，同时用数组使dog为五只

--- Chapter4_GoodDogSetter/GoodDog.java
+++ Chapter4_GoodDogSetter/GoodDog.java
@@ -1,41 +1,45 @@
+import java.util.Random;
+
 class GoodDog {
-	private int size;
+    private int[] sizes;
 
+    public int[] getSizes() {
+        return sizes;
+    }
 
-	public int getSize() {
-		return size;
-	}
+    public void setSizes(int[] sizes) {
+        this.sizes = sizes;
+    }
 
-	public void setSize(int s) {
-		size = s ;
-	}
-
-	void bark() {
-		if (size > 60) {
-			System.out.println("Wooof! Wooof!");
-		} else if (size > 14) {
-			System.out.println("Ruff!	Ruff!");
-		} else {
-			System.out.println("Yip!	Yip!");
-		}
-	}
+    public void bark() {
+        for (int size : sizes) {
+            if (size > 60) {
+                System.out.println("Wooof! Wooof!");
+            } else if (size > 30) {
+                System.out.println("Ruff! Ruff!");
+            } else {
+                System.out.println("Yip! Yip!");
+            }
+        }
+    }
 }
 
-
-
 class GoodDogTestDrive {
-	public static void main(String[] args) {
-		GoodDog one = new GoodDog();
-		one.setSize(70);
-		GoodDog two = new GoodDog();
-		two.setSize(8);
-		GoodDog three = new GoodDog();
-		three.setSize(30);
-		System.out.println("Dog one: " + one.getSize());
-		one.bark();
-		System.out.println("Dog two: " + two.getSize());
-		two.bark();
-		System.out.println("Dog three: " + three.getSize());
-		three.bark();
-	}
+    public static void main(String[] args) {
+        Random random = new Random();
+        GoodDog dog = new GoodDog();
+        int[] sizes = new int[5];
+        
+        for (int i = 0; i < 5; i++) {
+            sizes[i] = random.nextInt(101); 
+        }
+        
+        dog.setSizes(sizes);
+        
+        for (int size : dog.getSizes()) {
+            System.out.println("Dog size: " + size);
+        }
+        
+        dog.bark();
+    }
 }
