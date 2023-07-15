8b4f6e006a2aa9789356ab31749619daa38fbb7a
Ryan-Lou <1874216575@qq.com>
2023-07-16 03:36:22 +0800

    SImpleDotCom可输入版本
    添加了GameHelper类

--- Chapter5_SimpleDotCom/SimpleDotComTestDrive.java
+++ Chapter5_SimpleDotCom/SimpleDotComTestDrive.java
@@ -1,14 +1,29 @@
 public class SimpleDotComTestDrive {
 	
 	public static void main(String[] args) {
-		SimpleDotCom dot = new SimpleDotCom();
-		int[] locations = {2,3,4};
-		dot.setLocationCells(locations);
-		String userGuess = "2";
-		String result = dot.checkYourself(userGuess);
-		userGuess = "3";
-		 result = dot.checkYourself(userGuess);
-		userGuess = "4";
-		 result = dot.checkYourself(userGuess);
+
+		int numOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+
+
+		SimpleDotCom theDotCom = new SimpleDotCom();
+		int randNum = (int) (Math.random() *5);
+
+
+		int[] locations = {randNum,randNum+1,randNum+2};
+		theDotCom.setLocationCells(locations);
+		boolean isAlive = true;
+
+		while (isAlive == true) {
+			String guess = helper.getUserInput("enter a number");
+			String result = theDotCom.checkYourself(guess);
+			numOfGuesses++;
+			if (result.equals("kill")){
+				isAlive = false;
+				System.out.println("You took " + numOfGuesses + " guesses" );
+			}
+
+		}
+		
 	}
 }
